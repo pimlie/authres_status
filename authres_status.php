340f146768e9385ad2db55422de61209d20dc5ae
pimlie <pimlie@hotmail.com>
2017-06-07 19:38:10 +0200

    Updated version to 0.3.2

--- authres_status.php
+++ authres_status.php
@@ -4,7 +4,7 @@
  * This plugin displays an icon showing the status
  * of an authentication results header of the message
  *
- * @version 0.3.1
+ * @version 0.3.2
  * @author pimlie
  * @mail pimlie@hotmail.com
  *
