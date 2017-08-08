451fb59297967a77e0782934d7ec85d33d6cdd80
pimlie <pimlie@hotmail.com>
2017-08-08 23:21:14 +0200

    bump version

--- authres_status.php
+++ authres_status.php
@@ -4,7 +4,7 @@
  * This plugin displays an icon showing the status
  * of an authentication results header of the message
  *
- * @version 0.3.4
+ * @version 0.4.0
  * @author pimlie
  * @mail pimlie@hotmail.com
  *
