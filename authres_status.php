b35a92b6d277b4899086c5eb16b77e9572ddcc98
Pim <pimlie@hotmail.com>
2020-07-09 10:53:16 +0200

    chore: lint
    Co-authored-by: Andrey <soapAnd@gmail.com>

--- authres_status.php
+++ authres_status.php
@@ -67,7 +67,7 @@ class authres_status extends rcube_plugin
         $this->add_hook('storage_init', array($this, 'storage_init'));
         $this->add_hook('messages_list', array($this, 'messages_list'));
         $this->add_hook('message_headers_output', array($this, 'message_headers'));
-		$this->add_hook('template_object_messagesummary', array($this, 'message_summary'));
+        $this->add_hook('template_object_messagesummary', array($this, 'message_summary'));
 
         $dont_override = $rcmail->config->get('dont_override', array());
 
