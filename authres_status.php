bfb3f267d3f9a4c04a8550f655e94d3deb114901
Bernhard Lichtinger <bernhard.lichtinger@lrz.de>
2020-07-27 11:04:15 +0200

    Fix for https://github.com/pimlie/authres_status/issues/35

--- authres_status.php
+++ authres_status.php
@@ -142,7 +142,7 @@ class authres_status extends rcube_plugin
                     $args['blocks']['authresstatus']['name'] = $this->gettext('title_include_status');
 
                     $args['blocks']['authresstatus']['options']['enable' . $status]['title'] = $this->gettext('label_include_status' . $status);
-                    $input = new html_checkbox(array('name' => '_show_statuses[]', 'id' => 'enable_authres_status_column', 'value' => $status));
+                    $input = new html_checkbox(array('name' => '_show_statuses[]', 'id' => 'enable_authres_status_' . $status, 'value' => $status));
                     $args['blocks']['authresstatus']['options']['enable' . $status]['content'] = $input->show(($show_statuses & $status));
                 }
             }
